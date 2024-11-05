4221a03231e2f68d2aae1577ab7fa912305989f6
Mario Sousa <mario17sousa@hotmail.com>
2024-11-05 17:53:06 +0000

    PromptLogin

---
+++ PromptLoginAplication/src/io/codeforall/fanstatics/Main.java
@@ -0,0 +1,30 @@
+package io.codeforall.fanstatics;
+
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.string.PasswordInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
+    public class Main {
+        public static void main(String[] args) {
+            Prompt prompt = new Prompt(System.in, System.out);
+
+            System.out.println("Welcome to your first prompt-view Experience!");
+
+            System.out.println("Insert your login credentials below:");
+
+            StringInputScanner question1 = new StringInputScanner();
+            question1.setMessage("Username: ");
+
+            StringInputScanner question2 = new PasswordInputScanner();
+            question2.setMessage("Password: ");
+
+            String username = prompt.getUserInput(question1);
+            String password = prompt.getUserInput(question2);
+
+            if(username.equals("lhugens") && password.equals("cenas")){
+                System.out.println("\nLogin Successful!\nWelcome home, " + username + "!");
+            } else {
+                System.out.println("\nWrong Credentials! Exiting...");
+            }
+        }
+    }
